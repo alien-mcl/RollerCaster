29ffbb6cd885f9475b84b9e6e3bf0a01e45d0d57
alien-mcl <alien-mcl@users.noreply.github.com>
2017-09-12 20:18:44 +0200

    Initial tag version

--- .build/VersionAssemblyInfo.cs
+++ .build/VersionAssemblyInfo.cs
@@ -1,3 +1,3 @@
 [assembly: System.Reflection.AssemblyVersion("1.0.0.0")] 
 [assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")] 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.0-eeed0ae52f01c8c228262a2bd2e22c46890f14df")] 
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.0-b1707f5f9e26bfb8cb2bffd485fe4bb57a50ff66")] 
