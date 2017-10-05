b62c909002c62efaf208d2c1f4365bac09835094
alien-mcl <alien-mcl@users.noreply.github.com>
2017-10-05 21:56:54 +0200

    Added support for simple DTO classes.

--- RollerCaster.Tests/Data/SpecializedMulticastObject.cs
+++ RollerCaster.Tests/Data/SpecializedMulticastObject.cs
@@ -4,5 +4,18 @@
     {
         /// <summary>Gets or sets the identifier of this instance..</summary>
         public string Id { get; set; }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            var anotherInstance = obj as SpecializedMulticastObject;
+            return anotherInstance != null && Id.Equals(anotherInstance.Id);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
 }
