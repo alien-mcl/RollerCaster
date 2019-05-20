fe969a407f77d1c9e984d29cc50a6f721a55fc37
alien-mcl <alien-mcl@users.noreply.github.com>
2019-05-20 21:14:28 +0200

    Fixed nuspec

--- .build/VersionAssemblyInfo.cs
+++ .build/VersionAssemblyInfo.cs
@@ -1,3 +1,3 @@
-[assembly: System.Reflection.AssemblyVersion("1.2.0.0")] 
-[assembly: System.Reflection.AssemblyFileVersion("1.2.0.0")] 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.0-930cdb9403bdaf96743e305e9ba83c3bd558dfad")] 
+[assembly: System.Reflection.AssemblyVersion("1.2.3.0")] 
+[assembly: System.Reflection.AssemblyFileVersion("1.2.3.0")] 
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.3.0-930cdb9403bdaf96743e305e9ba83c3bd558dfad")] 
