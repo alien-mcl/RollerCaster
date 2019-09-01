8abe3b918603611eb3557b7bb4363adae27a70b0
alien-mcl <alien-mcl@users.noreply.github.com>
2019-09-01 21:14:40 +0200

    SOmething's wrong with versions

--- .build/VersionAssemblyInfo.cs
+++ .build/VersionAssemblyInfo.cs
@@ -1,3 +1,3 @@
-[assembly: System.Reflection.AssemblyVersion("1.2.8.0")] 
-[assembly: System.Reflection.AssemblyFileVersion("1.2.8.0")] 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.8.0")] 
+[assembly: System.Reflection.AssemblyVersion("1.2.7.0")] 
+[assembly: System.Reflection.AssemblyFileVersion("1.2.7.0")] 
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.7.0")] 
